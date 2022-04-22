cfb5c347f630f8067b486b526ef696b2a5b93551
Joona Gynther <joona.gynther@cgi.com>
2022-04-22 09:16:11 +0300

    Some styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,19 +26,19 @@ const Home: NextPage = () => {
                 <p className='text-lg mb-3'>Uusimmat kuitit</p>
                 <div className=''>
                   <div className='odd:bg-neutral-700 flex gap-2 p-1'>
-                    <p>Päivämäärä</p>
-                    <p>Yhtiö</p>
-                    <p>Summa</p>
+                    <p className='w-24'>Päivämäärä</p>
+                    <p className='w-24'>Yhtiö</p>
+                    <p className='w-24'>Summa</p>
                   </div>
                   <div className='odd:bg-neutral-700 flex gap-2 p-1'>
-                    <p>pp-kk-vvvv</p>
-                    <p>Esimerkki Oy</p>
-                    <p>xx,xx eur</p>
+                    <p className='w-24'>pp-kk-vvvv</p>
+                    <p className='w-24'>Esimerkki Oy</p>
+                    <p className='w-24'>xx,xx eur</p>
                   </div>
                   <div className='odd:bg-neutral-700 flex gap-2 p-1'>
-                    <p>pp-kk-vvvv</p>
-                    <p>Esimerkki Oy</p>
-                    <p>xx,xx eur</p>
+                    <p className='w-24'>pp-kk-vvvv</p>
+                    <p className='w-24'>Esimerkki Oy</p>
+                    <p className='w-24'>xx,xx eur</p>
                   </div>
                 </div>
               </div>
@@ -47,22 +47,22 @@ const Home: NextPage = () => {
                 <p className='text-lg mb-3'>Lähetetyt kuitit</p>
                 <div className=''>
                   <div className='odd:bg-neutral-700 flex gap-2 p-1'>
-                    <p>Päivämäärä</p>
-                    <p>Yhtiö</p>
-                    <p>Summa</p>
-                    <p>Vastaanottaja</p>
+                    <p className='w-24'>Päivämäärä</p>
+                    <p className='w-24'>Yhtiö</p>
+                    <p className='w-24'>Summa</p>
+                    <p className='w-24'>Vastaanottaja</p>
                   </div>
                   <div className='odd:bg-neutral-700 flex gap-2 p-1'>
-                    <p>pp-kk-vvvv</p>
-                    <p>Esimerkki Oy</p>
-                    <p>xx,xx eur</p>
-                    <p>Tilitoimisto Oy</p>
+                    <p className='w-24'>pp-kk-vvvv</p>
+                    <p className='w-24'>Esimerkki Oy</p>
+                    <p className='w-24'>xx,xx eur</p>
+                    <p className='w-28'>Tilitoimisto Oy</p>
                   </div>
                   <div className='odd:bg-neutral-700 flex gap-2 p-1'>
-                    <p>pp-kk-vvvv</p>
-                    <p>Esimerkki Oy</p>
-                    <p>xx,xx eur</p>
-                    <p>Verohallinto</p>
+                    <p className='w-24'>pp-kk-vvvv</p>
+                    <p className='w-24'>Esimerkki Oy</p>
+                    <p className='w-24'>xx,xx eur</p>
+                    <p className='w-24'>Verohallinto</p>
                   </div>
                 </div>
               </div>
@@ -74,6 +74,7 @@ const Home: NextPage = () => {
             <div className='px-4 py-10'>{thing}</div>
             )}
           </div>
+          <Footer/>
         </main>
       </div>
     </Wrapper>
@@ -98,14 +99,25 @@ const Navigation: React.FC = ({ children }) => {
   )
 }
 
+const Footer: React.FC = ({ children }) => {
+  return (
+    <footer className='flex justify-center py-4 px-4'>
+      (c) {new Date().getFullYear()} CGI
+    </footer>
+  )
+}
+
 const Sidebar: React.FC = ({ children }) => {
   return (
     <div>
       <div className="sticky top-0 z-10 w-56">
-        <div className='flex flex-col h-screen bg-neutral-800 p-4 divide-y divide-neutral-700'>
-          <div className='hover:bg-neutral-700 transition px-1 py-1 my-1 rounded'>Valikko 1</div>
-          <div className='hover:bg-neutral-700 transition px-1 py-1 my-1 rounded'>Valikko 2</div>
-          <div className='hover:bg-neutral-700 transition px-1 py-1 my-1 rounded'>Valikko 3</div>
+        <div className='flex flex-col h-screen bg-neutral-800 p-4'>
+          <div className='mb-5'>eKuittilomapakko</div>
+          <div className='divide-y divide-neutral-700'>
+            <div className='hover:bg-neutral-700 transition px-1 py-1 my-1 rounded'>Valikko 1</div>
+            <div className='hover:bg-neutral-700 transition px-1 py-1 my-1 rounded'>Valikko 2</div>
+            <div className='hover:bg-neutral-700 transition px-1 py-1 my-1 rounded'>Valikko 3</div>
+          </div>
         </div>
       </div>
     </div>
