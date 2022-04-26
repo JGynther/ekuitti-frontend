5422fd469587f6e29cbdab3746534af2256c299c
Joona Gynther <joona.gynther@cgi.com>
2022-04-26 16:44:44 +0300

    Initial connection via useReceipts hook

---
+++ utils/useReceipts/types.ts
@@ -0,0 +1,40 @@
+type Receipts = Receipt[];
+
+type Receipt = {
+  date: string;
+  company: {
+    name: string;
+    "business-id": string;
+    "vat-id": string;
+    address: {
+      street: string;
+      city: string;
+      "postal-code": string;
+      country: string;
+    };
+  };
+  products: Product[];
+  currency: string;
+  "total-price-excl-vat": number;
+  "vat-10"?: number;
+  "vat-14"?: number;
+  "vat-24"?: number;
+  "total-vat": number;
+  "total-price": number;
+  "meta-data"?: {
+    [key: string]: any;
+  };
+};
+
+type Product = {
+  name: string;
+  quantity: number;
+  unit: string;
+  "unit-price": number;
+  "price-total": number;
+  vat: number;
+  "unit-vat": number;
+  "vat-total": number;
+};
+
+export type { Product, Receipt, Receipts };
