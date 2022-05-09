92ce3e69f696707dc7df1ea844957e319660a06b
Joona Gynther <joona.gynther@cgi.com>
2022-05-09 09:30:05 +0300

    Fixed id based fetching

--- pages/receipts/[id].tsx
+++ pages/receipts/[id].tsx
@@ -1,15 +1,21 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Receipts from "components/Receipts";
+import { useRouter } from "next/router";
+import useRequest from "@utils/useRequest";
 
-const ReceiptPage: NextPage = ({children}) => {
+const ReceiptPage: NextPage = ({ children }) => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, isLoading, isError } = useRequest(`/receipts/${id}`);
+  console.log(id, data);
   return (
     <div>
       {/* Each page should have or inherit a head component. */}
       <Head>
         <title>Receipt</title>
       </Head>
-      <Receipts />
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
 };
