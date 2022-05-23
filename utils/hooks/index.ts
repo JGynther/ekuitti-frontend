480acd1ffd3faf6cfb6bbee6ebc1ee8cd1ea67d7
Joona Gynther <joona.gynther@cgi.com>
2022-05-23 13:25:59 +0300

    Add uniform export page for hooks

---
+++ utils/hooks/index.ts
@@ -0,0 +1,15 @@
+import * as usePost from "@utils/usePost";
+import * as useReceipts from "@utils/useReceipts";
+import * as useRequest from "@utils/useRequest";
+import * as useSafeRouter from "@utils/useSafeRouter";
+import { useAuth, useUser, useLogin } from "@utils/auth";
+
+export {
+  usePost,
+  useReceipts,
+  useRequest,
+  useSafeRouter,
+  useAuth,
+  useUser,
+  useLogin,
+};
