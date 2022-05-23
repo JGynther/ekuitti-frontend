0bee61a38e5c314cf3b779eb91fb4a61d8748776
Joona Gynther <joona.gynther@cgi.com>
2022-05-23 12:55:47 +0300

    Remove unnecesary Content component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,20 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Dashboard from "@components/Dashboard";
-import Content from "@components/Content";
-import Sidebar from "@components/Sidebar";
-import Footer from "@components/Footer";
-import Navigation from "@components/Navigation";
 import Header from "@components/Header";
 import { useLogin } from "@utils/auth";
 
 const Home: NextPage = () => {
   useLogin();
   return (
-    <div>
+    <>
       <Head>
         <title>eKuitti</title>
         <meta name="description" content="" />
       </Head>
       <Header />
-      <Content
-        sidebar={<Sidebar />}
-        navigation={<Navigation />}
-        footer={<Footer />}
-      >
-        <Dashboard />
-      </Content>
-    </div>
+      <Dashboard />
+    </>
   );
 };
 
