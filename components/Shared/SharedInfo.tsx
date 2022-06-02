8a6ebb5e7512c0bd311fb13a1d5f741b7b05aa09
akikop <aki.koppinen@metropolia.fi>
2022-06-02 18:55:25 +0300

    Changed naming and deleted redundant buttons

--- components/Shared/SharedInfo.tsx
+++ components/Shared/SharedInfo.tsx
@@ -1,4 +1,5 @@
 //import { useState } from "react";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
 const SharedInfo: React.FC<any> = ({ shared }) => {
   //const [showDetails, setShowDetails] = useState<Boolean>(false);
@@ -10,9 +11,9 @@ const SharedInfo: React.FC<any> = ({ shared }) => {
   return (
     <div onClick={() => handleClick()} className="flex px-4 py-[10px] items-center justify-between border-2 border-white hover:border-grey hover:cursor-pointer">
       <div>
-        <div className="text-receipt font-bold">Nimi: {shared.name}</div>
-        <div className="text-receipt font-bold">eOsoite: {shared.eAddressId}</div>
-        <div className="text-receipt font-bold">Kuitteja: {shared.sharedReceiptAmount} kpl</div>
+        <div className="text-receipt font-bold">{shared.name}</div>
+        <div className="text-dark">eOsoite: {shared.eAddressId}</div>
+        <div className="text-receipt font-bold mr-4">Kuitteja: {shared.sharedReceiptAmount} kpl</div>
       </div>
     </div>
   );
