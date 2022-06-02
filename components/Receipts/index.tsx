fc83e6e977ee351871c9f0e77f8490e6c11b1bd7
akikop <aki.koppinen@metropolia.fi>
2022-06-02 17:52:41 +0300

    Simple fix for data.map error after log out

--- components/Receipts/index.tsx
+++ components/Receipts/index.tsx
@@ -11,11 +11,15 @@ const Receipts: React.FC = () => {
     return <div>Loading...</div>;
   }
 
+  if (!data.map) {
+    return <div>Please refresh...</div>;
+  }
+
   return (
     <div>
       <div className="px-8 pt-6 pb-4 text-subtitle font-bold">Kaikki kuitit</div>
       <div className="px-10 overflow-y-scroll h-[calc(100vh-290px)]">
-        {data && data.map((receipt, index) => (
+        {data.map && data.map((receipt, index) => (
           <ReceiptInfo key={index} receipt={receipt} />
         ))}
       </div>
