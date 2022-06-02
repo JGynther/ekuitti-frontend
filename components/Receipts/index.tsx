84d0c9883f1adabcedb05f6b734ca802388bf568
Vilma Salmikuukka <vilma.salmikuukka@gmail.com>
2022-06-02 17:29:27 +0300

    Connect logout and redirect to login

--- components/Receipts/index.tsx
+++ components/Receipts/index.tsx
@@ -15,7 +15,7 @@ const Receipts: React.FC = () => {
     <div>
       <div className="px-8 pt-6 pb-4 text-subtitle font-bold">Kaikki kuitit</div>
       <div className="px-10 overflow-y-scroll h-[calc(100vh-290px)]">
-        {data?.map((receipt, index) => (
+        {data && data.map((receipt, index) => (
           <ReceiptInfo key={index} receipt={receipt} />
         ))}
       </div>
